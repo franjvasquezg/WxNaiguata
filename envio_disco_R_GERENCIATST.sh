aae660c272e54ef91e7c56f1b6346241f6b784de
franjvasquezg <franjvasquezg@gmail.com>
2020-04-14 15:44:25 -0400

    ajustes

--- envio_disco_R_GERENCIATST.sh
+++ envio_disco_R_GERENCIATST.sh
@@ -10,21 +10,20 @@ vPrefijo="/TDD/Entrada"
 vPrefijo_Res="/TDD/Entrada/Respaldo"
 COD_AMBIENTE2="Calidad"
 
-#vPrefijoWin=`echo $vPrefijo_Res | sed -e 's/\//\//'`   ## quita la primera "/"
-vPrefijoWin="\\TDD\\Entrada\\Respaldo"
-echo "vPrefijoWin  $vPrefijoWin"
+vPrefijoWin=`echo $vPrefijo | sed -e 's/\///'`   ## quita la primera "/"
+#vPrefijoWin="\TDD\\Entrada" #\\Respaldo"
 #echo "Inicio de Transferencia de archivo"
-vRUTAWIN1="\\TDD\\Entrada\\Respaldo\\${arch}"  #"$vPrefijoWin\\${arch}"
-echo                                                       ## $COD_AMBIENTE2   NUEVA VARIABLE DE AMBIENTE IPR1302
-echo "vRUTAWIN1 "$vRUTAWIN1
-echo
+vRUTAWIN1="$vPrefijoWin\\${arch}"  #"$vPrefijoWin\\${arch}"
 vRUTAWIN2="\\Procesos\\Compensacion Naiguata-MC\\${COD_AMBIENTE2}\\T461NA\\${arch}" ## $vpValRet_910\\$vArchDestB"
+echo       
+echo "vPrefijoWin  $vPrefijoWin"                                             ## $COD_AMBIENTE2   NUEVA VARIABLE DE AMBIENTE IPR1302
+echo "vRUTAWIN1 "$vRUTAWIN1
 echo "vRUTAWIN2 "$vRUTAWIN2
-echo
-echo "SFTP_USER}" ${SFTP_USER}
+echo "SFTP_USER "${SFTP_USER}
 echo "SFTP_IMC_NGTA" ${SFTP_IMC_NGTA}
 #vCOPIAWIN=`ssh ${SFTP_USER}@${SFTP_IMC_NGTA} cmd.exe /C "C:\\MFESFTP\\transtmve $vRUTAWIN1 $vPrefijoWin $vRUTAWIN2 $USUARIOTMVE $CLAVETMVE"`
-vCOPIAWIN=`ssh ${SFTP_USER}@${SFTP_IMC_NGTA} cmd.exe /R "R:\\dskclu02dt1\\GerenciaTsT-1 $vRUTAWIN1 $vPrefijoWin $vRUTAWIN2 $USUARIOTMVE $CLAVETMVE"`
+#vCOPIAWIN=`ssh ${SFTP_USER}@${SFTP_IMC_NGTA} cmd.exe /R "R:\\dskclu02dt1\\GerenciaTsT-1 $vRUTAWIN1 $vPrefijoWin $vRUTAWIN2 $USUARIOTMVE $CLAVETMVE"`
+vCOPIAWIN=`ssh ${SFTP_USER}@${SFTP_IMC_NGTA} cmd.exe /R "R:\\MFESFTP\\transtmve $vRUTAWIN1 $vPrefijoWin $vRUTAWIN2 $USUARIOTMVE $CLAVETMVE"`
 vSTATT=$?
 if [ "$vSTATT" != "0" ]
 then
@@ -36,4 +35,5 @@ then
 else
     echo "Archivo ${arch} transferido correctamente al Servidor SRVCCSALC (R)"
 fi
+
 
